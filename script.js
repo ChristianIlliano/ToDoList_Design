0f14b24d4cee74056e4b729abc3c79a558b1396c
ChristianIlliano <grafica.illiano@gmail.com>
2024-09-09 17:53:24 +0200

    feat pop-up and local storage

--- script.js
+++ script.js
@@ -6,22 +6,39 @@ createApp({
         return {
             newTask: {
                 taskText: "",
+                done: false,
             },
 
             tasks: [{
                 taskText: "ciaone",
+                done: true,
             }
         ],
         };
     },
 
     created() {
+        const localName = localStorage.getItem("username");
+        this.username = localName !== null ? localName : ""; 
 
+        const localTasks = localStorage.getItem("tasks");
+        if (localTasks !== null) {
+            this.tasks = JSON.parse(localTasks);
+        } else {
+            this.tasks = "";
+        }
     },
 
     methods: {
         AddTask: function() {
-            this.tasks.push(this.newTask);
-        }
+            this.tasks.push({...this.newTask});
+            const jsonTasks = JSON.stringify(this.tasks);
+            localStorage.setItem("tasks", jsonTasks);
+        },
+
+        save: function() {
+            localStorage.setItem("username", this.username);
+        },
     }
+
 }).mount("#app");
